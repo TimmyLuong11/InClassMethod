a167e4b909b1a457ccc56f32cf5d0f8a03820c16
Timothy Luong <Timothy.Luong@ou.edu>
2021-04-08 17:37:17 -0500

    Added another example

---
+++ Method_Functions/GradeMethod/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GradeMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade;
+            string answer , letterGrade;
+            Console.WriteLine("Please enter in your grade:");
+            answer = Console.ReadLine();
+            do
+            {
+                while (double.TryParse(answer, out grade) == false)
+                {
+                    Console.WriteLine("You did not enter in a number. Please try agian!");
+                    answer = Console.ReadLine();
+                }
+                letterGrade = GradeConverstion(grade);
+                Console.WriteLine($"Congrats you have a {letterGrade}");
+                Console.WriteLine("Do you want to convert another grade yes or no");
+                answer = Console.ReadLine();
+            } while (answer[0] == 'y');
+
+        }
+
+        /// <summary>
+        /// Converts a number grade to a letter grade
+        /// </summary>
+        /// <param name="grade"> A double reprsenting the grade </param>
+        /// <returns>
+        /// The letter grade associated with the grade and N/A if the grade is below 0
+        /// </returns>
+        static string GradeConverstion(double grade)
+        {
+            string letter = "";
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else if (grade >= 0)
+            {
+                letter = "F";
+            }
+            else
+            {
+                letter = "N/A";
+            }
+            return letter;
+        }
+    }
+}
---
+++ Method_Functions/Method_Functions/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Method_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> gpas = new List<double>();
+            List<double> itemPrices = new List<double>();
+            string response = "";
+            bool shouldLoopAgain = true;
+
+            while (shouldLoopAgain)
+            {
+                double gpa = ValidateInputIsADouble("Please enter in a GPA:");
+                gpas.Add(gpa);
+
+                shouldLoopAgain = DoesTheUserWantToGoAgain();
+                //Console.WriteLine("Do you want to enter another GPA? Type yes or no.");
+                //response = Console.ReadLine();
+            }
+            response = "";
+            shouldLoopAgain = true;
+            while(shouldLoopAgain) //(response != "no")
+            {
+                double gpa = ValidateInputIsADouble("Please enter the price of the item:");
+                itemPrices.Add(gpa);
+
+                shouldLoopAgain = DoesTheUserWantToGoAgain();
+                //Console.WriteLine("Do you want to enter another price? Type yes or no.");
+                //response = Console.ReadLine();
+            }
+            response = "";
+            shouldLoopAgain = true;
+            while (shouldLoopAgain)
+            {
+                double gpa = ValidateInputIsADouble("Please enter the price of the item:");
+                itemPrices.Add(gpa);
+
+                shouldLoopAgain = DoesTheUserWantToGoAgain();
+                //Console.WriteLine("Do you want to enter another price? Type yes or no.");
+                //response = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the user has more input to enter
+        /// </summary>
+        /// <returns> 
+        /// Returns true of the user is yes
+        /// Returns false if the user say no
+        /// </returns>
+        private static bool DoesTheUserWantToGoAgain()
+        {
+            string answer ="";
+            bool shouldContinue;
+            Console.WriteLine("Do you want to enter another value? yes or no");
+            answer = Console.ReadLine();
+            while (answer.ToLower() != "no" && answer.ToLower() != "yes")
+            {
+                Console.WriteLine("You must enter the word 'yes' or 'no'.");
+                Console.WriteLine("Do you want to enter another value yes or no");
+                answer = Console.ReadLine();
+            }
+            if (answer.ToLower() == "no")
+            {
+                shouldContinue = false;
+                //return false;
+            }
+            else
+            {
+                shouldContinue = true;
+                //return true;
+            }
+            return shouldContinue;
+        }
+
+        /// <summary>
+        /// Validates the user gives a valid double response for the question        
+        /// </summary>
+        /// <param name="message">The question to ask the user</param>
+        /// <returns>A valid double response</returns>
+        static double ValidateInputIsADouble(string message)
+        {
+            double value;
+            string answer;
+            do
+            {
+                Console.WriteLine(message);
+                answer = Console.ReadLine();
+                //bool isDouble = double.TryParse(answer, out value);
+            } while(double.TryParse(answer, out value) == false);
+
+            return value;
+        }
+    }
+}
